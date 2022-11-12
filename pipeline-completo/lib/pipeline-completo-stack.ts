f477234c873c2da495767e8ceee8342ae01c9d2c
Manuel Fuentes <manuel.fuentes@curantissolutions.com>
2022-11-12 00:09:11 -0600

    feat: initial structure

--- pipeline-completo/lib/pipeline-completo-stack.ts
+++ pipeline-completo/lib/pipeline-completo-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
+import { StackProps } from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { Options } from '../types/options';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+interface PipelineCompletoStackProps extends StackProps {
+  options: Options,
+}
+
 
 export class PipelineCompletoStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: PipelineCompletoStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PipelineCompletoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this,'TestPipeline',{
+      pipelineName: `${props?.options.pipelineName}`,
+      synth: new ShellStep('Synth',{
+        input: CodePipelineSource.gitHub(`${props?.options.rootAccount}/${props?.options.reposName}`,'main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth'
+        ]
+      })
+    });
   }
 }
